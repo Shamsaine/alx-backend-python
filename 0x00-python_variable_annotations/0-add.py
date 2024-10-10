3dcc2ee0320ff8b2afa8a2ee7ef8e404f55751ba
shamsudeen Ibrahim <shamsain3@gmail.com>
2024-10-10 16:06:16 +0000

    added second file concat.py and adjusted add.py to meet pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 ''' import module'''
 
+
 def add(a: float, b: float) -> float:
     '''Adds two floating numbers
     '''
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' import module'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''returns a string concatenation of str1
+    and str2
+    '''
+
+    return str1 + str2
