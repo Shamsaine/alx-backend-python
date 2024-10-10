0951d79dea959f2c1ae8a8596e51c49ca08c0f10
shamsudeen Ibrahim <shamsain3@gmail.com>
2024-10-10 16:19:51 +0000

    added 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''import module'''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''return the sum of the items in a list
+    '''
+    
+    return float(sum(input_list))
